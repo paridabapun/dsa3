c5a3cc8eb28e1ac2268534b4629d90b65c71857c
Bapun Parida <pariabapun@gmail.com>
2023-03-09 23:12:37 +0530

    Added

--- src/main/java/linkedlist1/LessonQuestion1.java
+++ src/main/java/linkedlist1/LessonQuestion1.java
@@ -1,5 +1,15 @@
 package linkedlist1;
 
+/*
+ * Access the kth element of LL (K >= 0)
+ */
 public class LessonQuestion1 {
+	public int solve(Node A, int B) {
+		Node temp = A;
 
+		for (int i = 0; i < B; i++) {
+			temp = temp.next;
+		}
+		return temp.data;
+	}
 }
---
+++ src/main/java/linkedlist1/Node.java
@@ -0,0 +1,13 @@
+package linkedlist1;
+
+public class Node {
+	
+	int data;
+	Node next;
+	Node(int x){
+		
+		this.data=x;
+		next=null;
+	}
+
+}
